2b6851e2d70e6e54bc912a61e04b13e85e124b5c
Xavier Chemin <xavier.chemin@viacesi.fr>
2020-03-30 15:04:25 +0200

    modification login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,19 @@
 // Get the modal
+$(document).ready(function () {
+    $("login").click(function () {
+        $("id01").show();
+    });
+});
 var modal = document.getElementById('id01');
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
+
+function validation() {
+    var username = document.getElementById("uname").value;
+    var password = document.getElementById("psw").value;
+
+};
