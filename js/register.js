99a0e5dcf895a205ab05edfdfe5fb8c4fb93da14
jrmy <jeremy.lecoz@hotmail.fr>
2020-03-30 17:11:34 +0200

    register

--- js/register.js
+++ js/register.js
@@ -1,10 +1,27 @@
 var modal = document.getElementById('id02');
+const linkHome = document.getElementById('nav-link-home');
+const closeRegisterFormIcon = document.getElementById('close-register-form-icon');
+
+closeRegisterFormIcon.addEventListener('click', function () {
+    closeRegisterForm();
+
+});
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target === modal) {
-        modal.style.display = "none";
+        closeRegisterForm = "none";
     }
 };
+
+function closeRegisterForm() {
+    modal.style.display = "none";
+    let lastActive = document.getElementsByClassName('nav-link-active');
+    if (lastActive[0] != ""){
+        lastActive[0].classList.remove('nav-link-active')
+    }
+    linkHome.classList.add('nav-link-active')
+}
 
 function validation() {
     var username = document.getElementById("uname").value;
