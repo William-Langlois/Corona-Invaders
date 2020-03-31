dd3bdd77c63e1a6ae190344a2d8995768bdb566b
xavier.chemin@viacesi.fr <xavier.chemin@viacesi.fr>
2020-03-31 15:03:42 +0200

    init fire base

--- js/login.js
+++ js/login.js
@@ -1,3 +1,31 @@
+(function () {
+
+    // Initialise Firebase
+    const config = {
+        apiKey: "AIzaSyBnBa-76xV5FN_pD6Rg-27Ad0bm3q3rYvQ",
+        authDomain: "corona-invaders.firebaseapp.com",
+        databaseURL: "https://corona-invaders.firebaseio.com",
+        projectId: "corona-invaders",
+        storageBucket: "corona-invaders.appspot.com",
+        messagingSenderId: "638389868093",
+        appId: "1:638389868093:web:23b50412f9e44b89da3c54",
+        measurementId: "G-HYRT2KCPF9"
+    };
+    firebase.initializeApp(config);
+
+    //Get elements
+    const txtEmail = document.getElementById("txtEmail");
+    const txtPassword = document.getElementById("txtPassword");
+    const btnLogin = document.getElementById("btnLogin");
+    const btnLogout = document.getElementById("nav-link-logout");
+
+
+
+});
+
+
+
+
 // Get the modal
 const modal = document.getElementById('id01');
 const linkHome = document.getElementById('nav-link-home');
@@ -30,3 +58,7 @@ function validation() {
     var password = document.getElementById("psw").value;
 
 }
+const auth = firebase.auth();
+auth.signInWithEmailAndPassword(email, pass);
+auth.createUserWithEmailAndPassword(email,pass);
+auth.onAuthStateChanged(firebaseUser =>{});
