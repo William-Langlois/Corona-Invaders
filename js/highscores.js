3481af735d6243908bef5e6d49cf7e148f7b1234
jrmy <jeremy.lecoz@hotmail.fr>
2020-04-02 19:14:32 +0200

    scoreboard

--- js/highscores.js
+++ js/highscores.js
@@ -1,6 +1,6 @@
-
-
-    const ulList = document.getElementById('scoreslist');
+    const namelist = document.getElementById('pname');
+    const scorelist = document.getElementById('pscore');
+    const accuracylist = document.getElementById('paccuracy');
     // init firebase
     const db = firebase.database();
     // Create references
@@ -19,14 +19,41 @@
             let accuracy = scores[k].accuracy;
             let score = scores[k].score;
             console.log(name, accuracy, score);
-            let li = document.createElement ('li');
-            li.innerHTML = (name + accuracy + score);
-            ulList.appendChild(li);
-            console.log(li)
+            let nameli = document.createElement ('li');
+            let scoreli = document.createElement('li');
+            let accuracyli = document.createElement('li');
+            nameli.innerHTML = (name);
+            scoreli.innerHTML = (score);
+            accuracyli.innerHTML = (accuracy);
+            namelist.appendChild(nameli);
+            scorelist.appendChild(scoreli);
+            accuracylist.appendChild(accuracyli);
+            console.log(nameli)
         }
+
+
     }
 
     function errData(err) {
         console.log('Error!');
         console.log(err);
+    }
+
+    window.onclick = function (event) {
+        if (event.target == document.getElementById('scoreboard')) {
+            closeScoreboard();
+        }
+    };
+
+    function closeScoreboard() {
+        let modal = document.getElementById('scoreboard');
+        let linkScoreboard = document.getElementById('nav-link-scoreboard');
+
+
+        modal.style.display = "none";
+        let lastActive = document.getElementsByClassName('nav-link-active');
+        if (lastActive[0] != "") {
+            lastActive[0].classList.remove('nav-link-active')
+        }
+        linkScoreboard.classList.add('nav-link-active')
     }
